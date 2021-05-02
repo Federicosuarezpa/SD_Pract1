c92ea4d5bb8db03f488cac21f4f157c44a62b4c1
federico <federicohernan.suarez@estudiants.urv.cat>
2021-05-02 09:19:44 +0200

    Commit final

--- client.py
+++ client.py
@@ -3,14 +3,14 @@ import xmlrpc.client
 
 import click
 
-
+# Mirar como hacer el requirements.txt, hacerlo con las librerías necesarias y probar con mas de dos # llamadas simultáneas. En principio funciona todo bien. Pero por si acaso juego de pruebas
 @click.command()
 @click.argument('type_task', nargs=1, required=True)
 @click.argument('task', nargs=1, required=True)
 @click.argument('url', default='[http://localhost:8000/]')
 @click.option('-n', default=1, type=int, nargs=1)
 def start_connection(type_task, task, url, n):
-    server = xmlrpc.client.ServerProxy("http://localhost:10000/", allow_none=True)
+    server = xmlrpc.client.ServerProxy("http://localhost:11000/", allow_none=True)
 
     if type_task == 'worker':
         if task == 'create':
